e36ae16b35da4f4a926a400b51591b931a3fcd6e
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-08-11 16:36:46 +0600

    nice try

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int num = 0;
+        int sum = 1;
+        while (num++ <= printToInclusive) {
+            System.out.println(sum);
+            sum*=num;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,9 @@
 package school.mjc.stage0.loops.task2;
 
+import java.util.Arrays;
+
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        System.out.print(String.valueOf(chars));
     }
 }
